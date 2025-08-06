5ab44c02ac17d5ed9fb27dbd8d60307e7d676c71
Kim Jaewoo <jaewoo1427@naver.com>
2025-08-06 23:25:41 +0900

    chapter 5

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -3,7 +3,12 @@ import Comment from "./Comment";
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"김재우"} comment={
+                "반가워요, 김재우입니다!:)"} />
+            <Comment name={"쿄주로"} comment={
+                "안녕하세요, 렌코쿠 쿄주로입니다?!!@@@@"} />
+            <Comment name={"나루토"} comment={
+                "안녕, 나루토야!!!"} />
         </div>
     );
 }
