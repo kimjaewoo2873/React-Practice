9cd8a51a70dbbd91c613dcf18a9256ed8aa98251
Kim Jaewoo <jaewoo1427@naver.com>
2025-08-26 19:51:40 +0900

    chapter 13

--- my-app/src/chapter_13/Card.jsx
+++ my-app/src/chapter_13/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Card(props) {
-    const { title, backgroundColor, children } = props;
+    const { title, backgroundColor, children } = props; // 비구조화 사용, 필요한 props만 뽑음, props.title이라고 작성 안 해도 괜찮
 
     return (
         <div style={{
--- my-app/src/chapter_13/ProfileCard.jsx
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -4,8 +4,8 @@ import Card from "./Card";
 function ProfileCard(props) {
     return (
         <Card title="Jaewoo Kim" backgroundColor="#4ea04e">
-            <p>안녕하세요, 저는 김재우입니다.</p>
-            <p>저는 리액트를 사용해 개발하고 있습니다.</p>
+            <p>안녕하세요, 저는 김재우입니다.</p> {/* children = 컴포넌트 태그 사이에 들어간 모든 JSX */}
+            <p>저는 리액트를 사용해 개발하고 있습니다.</p> {/* Card컴포넌트 안에 들어간 태그들은 모두 Card의 props의 children에 들어간다 */}
         </Card>
     );
 }
